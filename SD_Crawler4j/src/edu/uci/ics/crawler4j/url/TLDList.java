42111c4824a6010e6e6a88f82fceef1e4a2e559d
John K Lee <puremonkey2001@yahoo.com.tw>
2014-04-30 21:32:00 +0800

    - Update TLDList.java to load tld-names.txt internally

--- SD_Crawler4j/src/edu/uci/ics/crawler4j/url/TLDList.java
+++ SD_Crawler4j/src/edu/uci/ics/crawler4j/url/TLDList.java
@@ -8,7 +8,9 @@ import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 
 public class TLDList {
@@ -20,8 +22,20 @@ public class TLDList {
 	private TLDList() {
 		try {
 			// Change to search working folder
-			//InputStream stream = this.getClass().getClassLoader().getResourceAsStream(tldNamesFileName);
-			InputStream stream = new FileInputStream(new File(TldFilePath));						
+			//InputStream stream = this.getClass().getClassLoader().getResourceAsStream(tldNamesFileName);			
+			File externalFile = new File(TldFilePath);
+			InputStream stream = null;
+			if(externalFile.exists())
+			{
+				logger.debug(String.format("Loading %s externally...\n", TldFilePath));
+				stream = new FileInputStream(new File(TldFilePath));	
+			}
+			else
+			{
+				logger.debug(String.format("Loading %s internally...\n", TldFilePath));
+				stream = this.getClass().getResourceAsStream(TldFilePath);
+			}
+			
 			BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
 			String line;
 			while ((line = reader.readLine()) != null) 
@@ -54,4 +68,18 @@ public class TLDList {
 		return tldSet.contains(str);
 	}
 
+	
+	public static void main(String args[])
+	{
+		File log4j = new File("unitest_log4j.properties");
+		if(log4j.exists())
+		{
+			PropertyConfigurator.configure(log4j.getAbsolutePath()); 
+		}
+		else
+		{
+			BasicConfigurator.configure();  
+		}
+		TLDList tlds = TLDList.getInstance();
+	}
 }
